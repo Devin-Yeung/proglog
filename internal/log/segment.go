746c2eb456602d998a45895e98566182197056a5
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2026-01-30 02:57:19 +0800

    feat(log): implement the segment layer

---
+++ internal/log/segment.go
@@ -0,0 +1,130 @@
+package log
+
+import (
+	"fmt"
+	"os"
+	"path"
+
+	api "github.com/Devin-Yeung/proglog/api/v1"
+	"google.golang.org/protobuf/proto"
+)
+
+// segment represents a log segment, which consists of a store and an index.
+type segment struct {
+	// store is the store associated with this segment.
+	store *store
+	// index is the index associated with this segment.
+	index *index
+	// config holds the configuration for this segment.
+	config Config
+	// baseOffset is the starting point of this segment.
+	baseOffset uint64
+	// nextOffset is the right boundary (exclusive) of this segment.
+	nextOffset uint64
+}
+
+// newSegment creates a new segment in the specified directory with the given base offset and configuration. The file
+// names for the store and index are derived from the base offset. If index or store are missing, they will be created.
+func newSegment(dir string, baseOffset uint64, config Config) (*segment, error) {
+	s := &segment{
+		baseOffset: baseOffset,
+		config:     config,
+	}
+
+	// handle the store
+	storePath := path.Join(dir, fmt.Sprintf("%d.store", baseOffset))
+	storeFile, err := os.OpenFile(storePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	if s.store, err = newStore(storeFile); err != nil {
+		return nil, err
+	}
+
+	// handle the index
+	indexPath := path.Join(dir, fmt.Sprintf("%d.index", baseOffset))
+	indexFile, err := os.OpenFile(indexPath, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	if s.index, err = newIndex(indexFile, config); err != nil {
+		return nil, err
+	}
+
+	// fetch the right boundary offset from the index
+	if lastEntryOffset, _, err := s.index.Read(-1); err != nil {
+		// todo: error is always EOF here?
+		s.nextOffset = baseOffset // index is empty if eof
+	} else {
+		s.nextOffset = baseOffset + uint64(lastEntryOffset) + 1
+	}
+
+	return s, nil
+}
+
+// Append adds a new record to the segment and returns the offset of the appended record.
+func (s *segment) Append(record *api.Record) (offset uint64, err error) {
+	cur := s.nextOffset
+	record.Offset = cur
+
+	// serialize the record
+	p, err := proto.Marshal(record)
+	if err != nil {
+		return 0, err
+	}
+
+	// append to the store
+	_, pos, err := s.store.Append(p)
+	if err != nil {
+		return 0, nil
+	}
+
+	// append to the index
+	relativeOffset := uint32(s.nextOffset - s.baseOffset)
+	if err = s.index.Write(relativeOffset, pos); err != nil {
+		return 0, err
+	}
+
+	s.nextOffset += 1
+	return cur, nil
+}
+
+// Read retrieves a record from the segment at the specified **absolute** offset.
+func (s *segment) Read(offset uint64) (*api.Record, error) {
+	relativeOffset := int64(offset - s.baseOffset)
+
+	// retrieve the position from the index
+	_, pos, err := s.index.Read(relativeOffset)
+	if err != nil {
+		return nil, err
+	}
+
+	// read the record from the store
+	p, err := s.store.Read(pos)
+	if err != nil {
+		return nil, err
+	}
+
+	// unmarshal the record
+	record := &api.Record{}
+	if err = proto.Unmarshal(p, record); err != nil {
+		return nil, err
+	}
+
+	return record, nil
+}
+
+// Close closes the segment's store and index.
+func (s *segment) Close() error {
+	if err := s.index.Close(); err != nil {
+		return err
+	}
+
+	if err := s.store.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ internal/log/segment_test.go
@@ -0,0 +1,54 @@
+package log
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"testing"
+
+	api "github.com/Devin-Yeung/proglog/api/v1"
+	"github.com/docker/go-units"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSegment(t *testing.T) {
+	tmpdir := t.TempDir()
+	defer os.RemoveAll(tmpdir)
+
+	c := Config{}
+	c.Segment.MaxIndexBytes = 1 * units.MiB
+	c.Segment.MaxStoreBytes = 10 * units.MiB
+
+	baseOffset := rand.Int()
+
+	s, err := newSegment(tmpdir, uint64(baseOffset), c)
+	require.NoError(t, err)
+
+	for i := 0; i < 10; i++ {
+		want := &api.Record{Value: []byte(fmt.Sprintf("%d", baseOffset+i))}
+		// append the record to the segment
+		offset, err := s.Append(want)
+		require.NoError(t, err)
+		require.Equal(t, uint64(baseOffset+i), offset)
+
+		// read the record back from the segment
+		got, err := s.Read(offset)
+		require.NoError(t, err)
+		require.Equal(t, want.Value, got.Value)
+	}
+	err = s.Close()
+	require.NoError(t, err)
+
+	// reopen the segment
+	s, err = newSegment(tmpdir, uint64(baseOffset), c)
+	require.NoError(t, err)
+	defer s.Close()
+
+	// health check the reopened segment
+	for i := 0; i < 10; i++ {
+		want := &api.Record{Value: []byte(fmt.Sprintf("%d", baseOffset+i))}
+		got, err := s.Read(uint64(baseOffset + i))
+		require.NoError(t, err)
+		require.Equal(t, want.Value, got.Value)
+	}
+}
