05b71898a18f5c7aca38c10abf6af300419ce49e
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2026-02-04 17:24:22 +0800

    feat(server): add gRPC ConsumeStream and test streaming consumption

--- internal/server/server.go
+++ internal/server/server.go
@@ -49,6 +49,40 @@ func (s *grpcLogServer) Consume(ctx context.Context, req *api.ConsumeRequest) (*
 	return &api.ConsumeResponse{Record: record}, nil
 }
 
+func (s *grpcLogServer) ConsumeStream(req *api.ConsumeRequest, stream api.Log_ConsumeStreamServer) error {
+	cursor := req.Offset
+
+	for {
+		select {
+
+		case <-stream.Context().Done():
+			return nil
+
+		default:
+			record, err := s.Consume(
+				stream.Context(),
+				&api.ConsumeRequest{Offset: cursor},
+			)
+
+			switch err.(type) {
+			case nil:
+				// ok
+			case api.ErrOffsetOutOfRange:
+				// wait until new records are appended
+				continue
+			default:
+				return err
+			}
+
+			if err := stream.Send(record); err != nil {
+				return err
+			}
+			cursor += 1
+		}
+	}
+
+}
+
 func NewGRPCServer(config *Config) (*grpc.Server, error) {
 	gsrv := grpc.NewServer()
 	// create the log service
--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -2,8 +2,10 @@ package server
 
 import (
 	"context"
+	"fmt"
 	"net"
 	"testing"
+	"time"
 
 	api "github.com/Devin-Yeung/proglog/api/v1"
 	"github.com/Devin-Yeung/proglog/internal/log"
@@ -21,6 +23,7 @@ func TestLog(t *testing.T) {
 
 	for _, tc := range []testCase{
 		{name: "produce/consume", fn: testProduceConsume},
+		{name: "consume stream", fn: testConsumeStream},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
 			client, tearDown := setupTestServer(t)
@@ -101,4 +104,42 @@ func testProduceConsume(t *testing.T, client api.LogClient) {
 	require.NoError(t, err)
 	assert.Equal(t, want.Value, consumeResp.Record.Value)
 	assert.Equal(t, produceResp.Offset, consumeResp.Record.Offset)
+}
+
+func testConsumeStream(t *testing.T, client api.LogClient) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// spin a new go routine to produce records
+	go func() {
+		for i := 0; i < 10000; i++ {
+			record := &api.Record{
+				Value:  []byte(fmt.Sprintf("offset %d", i)),
+				Offset: uint64(i),
+			}
+
+			_, err := client.Produce(
+				ctx,
+				&api.ProduceRequest{Record: record},
+			)
+			require.NoError(t, err)
+		}
+	}()
+
+	consumeStream, err := client.ConsumeStream(
+		ctx,
+		&api.ConsumeRequest{Offset: 0},
+	)
+	require.NoError(t, err)
+
+	// should always receive all records
+	for i := 0; i < 10000; i++ {
+		select {
+		case <-ctx.Done():
+			t.Fatal("test timed out")
+		default:
+			_, err := consumeStream.Recv()
+			assert.NoError(t, err)
+		}
+	}
 }
