013ac795a05df1a0510906805901842f7a33050d
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2026-02-06 13:36:48 +0800

    chore: bumping to casbin v3

--- internal/auth/authorizer.go
+++ internal/auth/authorizer.go
@@ -3,7 +3,7 @@ package auth
 import (
 	"fmt"
 
-	"github.com/casbin/casbin"
+	"github.com/casbin/casbin/v3"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -13,14 +13,20 @@ type Authorizer struct {
 }
 
 // NewAuthorizer creates a new Authorizer with the given model and policy files.
-func NewAuthorizer(model, policy string) *Authorizer {
-	enforcer := casbin.NewEnforcer(model, policy)
-	return &Authorizer{enforcer: enforcer}
+func NewAuthorizer(model, policy string) (*Authorizer, error) {
+	enforcer, err := casbin.NewEnforcer(model, policy)
+	if err != nil {
+		return nil, err
+	}
+	return &Authorizer{enforcer: enforcer}, nil
 }
 
 // Authorize checks if the subject is allowed to perform the action on the object.
 func (a *Authorizer) Authorize(subject, object, action string) error {
-	allowed := a.enforcer.Enforce(subject, object, action)
+	allowed, err := a.enforcer.Enforce(subject, object, action)
+	if err != nil {
+		return status.Errorf(codes.Internal, "authorize request: %v", err)
+	}
 	if !allowed {
 		msg := fmt.Sprintf("%s is not allowed to %s on %s", subject, action, object)
 		st := status.New(codes.PermissionDenied, msg)
